b8577cc2dea4ccb07ffaa88eba61047bc00a209c
Romulo Gomes <romulo.gomes@cscglobal.com>
2026-07-10 18:49:54 -0300

    implementação dos testes de de edição e deleção de produtos

--- cypress/support/pageObjects/navPage.js
+++ cypress/support/pageObjects/navPage.js
@@ -1,11 +1,15 @@
 export default class NavPage {
     constructor() {
         this.cadastroProdutoButton = '[data-testid="cadastrar-produtos"]';
-        this.listaProdutosButton = '[data-testid="lista-produtos"]';
+        this.listaProdutosButton = '[data-testid="listar-produtos"]';
         this.logoutButton = '[data-testid="logout"]';
     }
 
     accessCadastroProdutosPage() {
         cy.get(this.cadastroProdutoButton).click();
+    }
+
+    accessListaProdutosPage() {
+        cy.get(this.listaProdutosButton).click();
     }
 }
