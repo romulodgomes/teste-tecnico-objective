e83b75dd216298be0940be938a392174e90de0de
Romulo Gomes <romulo.gomes@cscglobal.com>
2026-07-10 23:25:00 -0300

    parametrizacao da validação de imagem dos produtos

--- cypress/support/pageObjects/listarProdutosPage.js
+++ cypress/support/pageObjects/listarProdutosPage.js
@@ -1,16 +1,17 @@
 export default class ListarProdutosPage {
     constructor() {
         this.produtosTable = 'table tr';
+        this.defaultImagePrefix = 'C:\\fakepath\\';
     }
 
-    verifyProdutoCreated( nome, preco, descricao, quantidade ) {
+    verifyProdutoCreated( nome, preco, descricao, quantidade, imagem ) {
         const linhaProduto = cy.get(this.produtosTable).contains(nome).parent('tr');
         linhaProduto.within(() => {
             cy.get('td').eq(0).should('have.text', nome);
             cy.get('td').eq(1).should('have.text', preco);
             cy.get('td').eq(2).should('have.text', descricao);
             cy.get('td').eq(3).should('have.text', quantidade);
-            cy.get('td').eq(4).should('have.text', 'C:\\fakepath\\test-img.png');
+            cy.get('td').eq(4).should('have.text', this.defaultImagePrefix + imagem.split('/').pop());
         });
     }
 
